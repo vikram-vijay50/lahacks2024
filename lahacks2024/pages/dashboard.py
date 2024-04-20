380f6fbacbf31fa45b1097806539d587ccfe6030
durpdur <durpdur@gmail.com>
2024-04-20 03:03:04 -0700

    Venv update

--- lahacks2024/pages/dashboard.py
+++ lahacks2024/pages/dashboard.py
@@ -14,12 +14,5 @@ def dashboard() -> rx.Component:
     """
     return rx.vstack(
         rx.heading("Upload a video", size="8"),
-        rx.upload(
-        rx.text(
-            "Drag and drop files here or click to select files"
-        ),
-        id="my_upload",
-        border="1px dotted rgb(107,99,246)",
-        padding="5em",
+        
     )
-)
--- lahacks2024/pages/index.py
+++ lahacks2024/pages/index.py
@@ -35,6 +35,12 @@ color = "rgb(107,99,246)"
 def index() -> rx.Component:
     """The main view."""
     return rx.vstack(
+        rx.vstack(
+            rx.heading(
+                "Upload your footage here!",
+                size="8"
+            ),
+        ),
         rx.upload(
             rx.vstack(
                 rx.button("Select File", color=color, bg="white", border=f"1px solid {color}"),
@@ -45,6 +51,10 @@ def index() -> rx.Component:
             padding="5em",
         ),
         rx.hstack(rx.foreach(rx.selected_files("upload1"), rx.text)),
+        rx.button(
+            "Upload",
+            on_click=State.handle_upload(rx.upload_files(upload_id="upload1")),
+        ),
         rx.button(
             "Clear",
             on_click=rx.clear_selected_files("upload1"),
